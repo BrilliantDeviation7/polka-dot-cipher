11fd5911dfff100808fbbce023513fd32d8ba8da
Kevin Zhu <54754524+BrilliantDeviation7@users.noreply.github.com>
2023-04-21 14:02:41 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import PolkaDotGroup from "./lib/PolkaDotGroup.svelte";
+
+  $: groups = [1, 1, 1, 1, 1, 1];
+
+  function addGroup() {
+    groups = [...groups, 1];
+  }
+</script>
+
+<main>
+  <p>
+    By <a
+      href="https://github.com/BrilliantDeviation7"
+      target="_blank"
+      rel="noreferrer">BrilliantDeviation7</a
+    >. See the
+    <a
+      href="https://github.com/BrilliantDeviation7/polka-dot-cipher"
+      target="_blank"
+      rel="noreferrer">repository</a
+    >.
+  </p>
+
+  <div class="card">
+    <button on:click={addGroup}>Add Polka Dot Group</button>
+  </div>
+
+  <div class="groups">
+    {#each groups as group}
+      <PolkaDotGroup />
+    {/each}
+  </div>
+</main>
+
+<style>
+  .groups {
+    display: flex;
+    column-gap: 40px;
+    width: screen;
+    flex-wrap: wrap;
+  }
+  button {
+    padding: 2em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  button:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+</style>
---
+++ src/lib/PolkaDotGroup.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import key from "../assets/key.json";
+  const status = [false, false, false, false, false];
+  function handleClick(event) {
+    let id = Number(event.target.id);
+    status[id] = !status[id];
+  }
+</script>
+
+<div class="container">
+  <div class="left">
+    <div id="0" class="dot" on:click={handleClick} class:active={status[0]} />
+    <div id="1" class="dot" on:click={handleClick} class:active={status[1]} />
+  </div>
+  <div class="right">
+    <div id="2" class="dot" on:click={handleClick} class:active={status[2]} />
+    <div id="3" class="dot" on:click={handleClick} class:active={status[3]} />
+    <div id="4" class="dot" on:click={handleClick} class:active={status[4]} />
+  </div>
+  <div style="margin-left: auto; margin-right: auto; font-size: 20px;">
+    <!-- {status.map((e) => (e ? 1 : 0))} -->
+    {key[status.map((e) => (e ? 1 : 0)).join("")]}
+  </div>
+</div>
+
+<style>
+  .container {
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    width: 90px;
+  }
+
+  .left {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .dot {
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    background-color: #f5f3f5;
+    transition: background-color 0.3s ease, transform 0.3s ease;
+    margin-top: 8px;
+    cursor: pointer;
+    border: 2px solid #1b264f;
+  }
+
+  .dot:hover {
+    transform: scale(1.1);
+  }
+
+  .active {
+    background-color: #1b264f;
+  }
+</style>
